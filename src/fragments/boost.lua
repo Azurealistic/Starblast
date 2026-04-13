bf2c867da1f18a2ee5c22f3c94b3f7b75203de50
Azurealistic <muhammadasavir@outlook.com>
2026-04-13 17:16:54 -0400

    Try and implement boost

--- src/fragments/boost.lua
+++ src/fragments/boost.lua
@@ -2,7 +2,7 @@ local ecs = require "libs.evolved"
 
 local boost = ecs.builder()
     :name('fragments.size')
-    :default(false) -- true if boost enabled, false if not
+    :default(false) -- True = Boost enabled!
     :spawn()
 
 return boost
