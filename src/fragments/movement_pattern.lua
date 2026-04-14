d687f89d75a74269daff82fa58cd427cf657a360
Azurealistic <muhammadasavir@outlook.com>
2026-04-13 21:34:40 -0400

    Game mostly done!

--- src/fragments/movement_pattern.lua
+++ src/fragments/movement_pattern.lua
@@ -7,11 +7,15 @@ local id = ecs.builder()
 
 return {
     id           = id,
-    STRAIGHT     = 0,  -- falls straight down
-    SINE         = 1,  -- gentle side-to-side weave
-    ZIGZAG       = 2,  -- sharp direction cuts
-    DIVE_LEFT    = 3,  -- sweeps hard left then straightens
-    DIVE_RIGHT   = 4,  -- sweeps hard right then straightens
-    STRAFE_LEFT  = 5,  -- enters from right, glides left-down then drops
-    STRAFE_RIGHT = 6,  -- enters from left, glides right-down then drops
+    STRAIGHT     = 0,   -- falls straight down
+    SINE         = 1,   -- gentle side-to-side weave
+    ZIGZAG       = 2,   -- sharp direction cuts
+    DIVE_LEFT    = 3,   -- sweeps hard left then straightens
+    DIVE_RIGHT   = 4,   -- sweeps hard right then straightens
+    STRAFE_LEFT  = 5,   -- glides left-down then drops
+    STRAFE_RIGHT = 6,   -- glides right-down then drops
+    SWEEP_LEFT   = 7,   -- fast horizontal sweep right-to-left, then drops
+    SWEEP_RIGHT  = 8,   -- fast horizontal sweep left-to-right, then drops
+    ORBIT        = 9,   -- circular spiral descent
+    CHARGE       = 10,  -- slow hover then sudden fast dive
 }
